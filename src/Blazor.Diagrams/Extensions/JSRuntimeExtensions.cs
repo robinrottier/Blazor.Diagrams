4a06797f7b7bdba43b1323a3a31e4634943689e6
Robin Rottier <robin@rottier.uk>
2026-02-06 11:18:25 +0000

    Fix issue with invalidoperations exception (and other types) when call remote JSRuntime interop. Happens when refresh a page in server side rendering

--- src/Blazor.Diagrams/Extensions/JSRuntimeExtensions.cs
+++ src/Blazor.Diagrams/Extensions/JSRuntimeExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using Blazor.Diagrams.Core.Geometry;
+﻿using Blazor.Diagrams.Core.Geometry;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
@@ -16,18 +14,41 @@ public static class JSRuntimeExtensions
     public static async Task ObserveResizes<T>(this IJSRuntime jsRuntime, ElementReference element,
         DotNetObjectReference<T> reference) where T : class
     {
-        try
-        {
-            await jsRuntime.InvokeVoidAsync("ZBlazorDiagrams.observe", element, reference, element.Id);
-        }
-        catch (ObjectDisposedException)
-        {
-            // Ignore, DotNetObjectReference was likely disposed
-        }
+        await SafeJSRuntimeInvokeVoidAsync(jsRuntime, "ZBlazorDiagrams.observe", element, reference, element.Id);
     }
 
     public static async Task UnobserveResizes(this IJSRuntime jsRuntime, ElementReference element)
     {
-        await jsRuntime.InvokeVoidAsync("ZBlazorDiagrams.unobserve", element, element.Id);
+        await SafeJSRuntimeInvokeVoidAsync(jsRuntime, "ZBlazorDiagrams.unobserve", element, element.Id);
+    }
+
+    private static async Task SafeJSRuntimeInvokeVoidAsync(IJSRuntime jsRuntime, string identifier, params object?[]? args)
+    {
+        //
+        // protect js runtime calls against runtime being uninitialized, disconnected or disposed or otherwise failing
+        // which can happen when the user navigates away from the page or when the component is disposed
+        // .net10 seems to introduce InvalidOperationException when simply reloading a server side render page. The exception says:
+        //      JavaScript interop calls cannot be issued at this time. This is because the component is being statically rendered.
+        //      When prerendering is enabled, JavaScript interop calls can only be performed during the OnAfterRenderAsync lifecycle method.
+        //
+        // perhaps a catch-all should be added -- do we care if a void task in remote runtime fails in any way?
+        //
+        // (in .net10) jsRuntime is a Microsoft.AspNetCore.Components.Server.Circuits.RemoteJSRuntime 
+        // - it has ".IsInitialized" which seems to be false when this fails so that could be tested before call to avois exceptin needing to be caught
+        // - but dont know how to reference it and reflection may be too much here
+        //
+        try
+        {
+            await jsRuntime.InvokeVoidAsync(identifier, args);
+        }
+        catch (Exception ex) when (
+                   ex is ObjectDisposedException 
+                || ex is InvalidOperationException 
+                || ex is JSDisconnectedException
+            )
+        {
+            // This can happens alot in some "normal" operations,
+            // System.Diagnostics.Debug.WriteLine($"Ignore {ex.GetType().FullName} from JSRuntime.{identifier}");
+        }
     }
 }
