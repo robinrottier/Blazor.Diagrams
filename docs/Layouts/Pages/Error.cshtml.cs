485197b7ec72fef0484b9744a51690fbb28451aa
Robin Rottier <robin@rottier.uk>
2026-02-28 12:20:21 +0000

    RequestId can be null

--- docs/Layouts/Pages/Error.cshtml.cs
+++ docs/Layouts/Pages/Error.cshtml.cs
@@ -9,7 +9,7 @@ namespace Layouts.Pages;
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
-public string RequestId { get; set; }
+public string? RequestId { get; set; }
 
 public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
